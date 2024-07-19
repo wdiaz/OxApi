53ae34b0ec22b69130feeb48eebf161e2393edfb
Walter Diaz <wdiaz503@gmail.com>
2024-07-19 23:28:02 +0000

    added a very non usefull test - placeholder to test funtionality only

---
+++ src/test/java/com/commerce/api/controller/CartControllerTest.java
@@ -0,0 +1,13 @@
+package com.commerce.api.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartControllerTest {
+
+    @Test
+    public void testOneEqualsOne() {
+        assertEquals(1, 1, "1 should be equal to 1");
+    }
+}
