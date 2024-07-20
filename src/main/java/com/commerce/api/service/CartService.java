a6da7ce7d2927c523a3d364cf32fb92d62de4ed5
Walter Diaz <wdiaz503@gmail.com>
2024-07-20 04:31:23 +0000

    implemented delete cart in cart service

--- src/main/java/com/commerce/api/service/CartService.java
+++ src/main/java/com/commerce/api/service/CartService.java
@@ -2,52 +2,35 @@ package com.commerce.api.service;
 
 import com.commerce.api.entity.Cart;
 import com.commerce.api.entity.CartItem;
-import com.commerce.api.entity.Merchant;
 import com.commerce.api.entity.Product;
 import com.commerce.api.repository.CartRepository;
-import com.commerce.api.repository.MerchantRepository;
 import com.commerce.api.repository.ProductRepository;
+import com.commerce.api.service.cart.CartFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
+
 import java.time.Instant;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class CartService {
 
     ProductRepository productRepository;
 
-    MerchantRepository merchantRepository;
-
     CartRepository cartRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(CartService.class);
+
     @Autowired
-    public CartService(ProductRepository productRepository, MerchantRepository merchantRepository, CartRepository cartRepository) {
+    public CartService(ProductRepository productRepository, CartRepository cartRepository) {
         this.productRepository = productRepository;
-        this.merchantRepository = merchantRepository;
         this.cartRepository = cartRepository;
     }
-
-    /*public Cart createCart(Long productId, Long merchantId) {
-        Merchant merchant = merchantRepository.findById(merchantId)
-                .orElseThrow(() -> new RuntimeException("Merchant not found"));
-
-        Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new RuntimeException("Product not found"));
-
-        UUID uuid = UUID.randomUUID();
-        Cart cart = new Cart();
-        cart.setUuid(uuid.toString());
-        cart.setStatus(Cart.CREATED);
-        cart.setSession("34523523523532453352");
-        cartRepository.save(cart);
-        return cart;
-    }*/
-
     public Cart findByUuid(String uuid) {
         return cartRepository.findByUuid(uuid).orElseThrow(() -> new RuntimeException("Cart not found"));
     }
@@ -58,20 +41,20 @@ public class CartService {
 
     @Transactional
     public Cart addCartItem(String uuid, Long productId, Integer quantity) {
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("Product not found with id: " + productId));
+
         Optional<Cart> optionalCart = cartRepository.findByUuid(uuid);
+
         Cart cart = optionalCart.orElseGet(() -> {
-            Cart newCart = new Cart();
-            newCart.setUuid(UUID.randomUUID().toString());
+            Cart newCart = CartFactory.createNew();
+            newCart.setUuid(uuid);
             newCart.setStatus(Cart.CREATED);
-
-
             UUID randomUUID = UUID.randomUUID();
             newCart.setSession(randomUUID.toString());
             return newCart;
         });
-
-        Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new IllegalArgumentException("Product not found with id: " + productId));
 
         // Check if the product already exists in the cart
         boolean productExists = cart.getCartItems().stream()
@@ -104,13 +87,28 @@ public class CartService {
         }
         cartRepository.save(cart);
         return cart;
+    }
 
-        /*UUID randomUUID = UUID.randomUUID();
-        Cart cart = new Cart();
-        cart.setUuid(randomUUID.toString());
-        cart.setStatus(Cart.CREATED);
-        cart.setSession("34523523523532453352");
-        cartRepository.save(cart);
-        return cart;*/
+
+    public Map<String, Object> deleteCartItem(String cartUuid, Long productId) {
+        Cart cart = findByUuid(cartUuid);
+
+        Optional<CartItem> existingCartItem = cart.getCartItems().stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst();
+
+        cart.getCartItems().removeIf(item -> item.getProduct().getId().equals(productId));
+        if (existingCartItem.isPresent()) {
+            CartItem cartItem = existingCartItem.get();
+            cart.getCartItems().remove(cartItem);
+            cartRepository.save(cart);
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("message", "Item deleted successfully");
+            response.put("cart", cart);
+            return response;
+        } else {
+            throw new NoSuchElementException("Item not found in cart");
+        }
     }
 }
